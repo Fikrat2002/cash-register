7616c1d1739b8fd35ad79529ed0e427e310acf47
fikratkhatira <fekratkhatira@gmail.com>
2024-07-10 12:43:50 -0700

    html codes cleaned, css and js errors fixed

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-let cash = document.getElementById('cash');
-let displayChangeDue = document.getElementById('change-due');
-let purchaseBtn = document.getElementById('purchase-btn');
-let displayCid = document.getElementById('cash-in-drawer');
-
-let price = 3.26;
+const cash = document.getElementById('cash');
+const displayChangeDue = document.getElementById('change-due');
+const purchaseBtn = document.getElementById('purchase-btn');
+const displayCid = document.getElementById('cash-in-drawer');
+const price = 3.26;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
 
-document.getElementById("price").innerHTML = `<b>Price:</b> ${price}`;
+document.getElementById('price').innerHTML = `<b>Price:</b> ${price}`;
 
 const checkRegister = () => {
-  let cashInt = parseFloat(cash.value);
+  const cashInt = parseFloat(cash.value);
   let change = Number((cashInt - price).toFixed(2));
-  let totalCid = Number(cid.reduce((total, sum) => total + sum[1], 0).toFixed(2));
+  const totalCid = Number(cid.reduce((total, sum) => total + sum[1], 0).toFixed(2));
 
-  document.getElementById("change").innerHTML = `<b>Change:</b> ${change}`;
+  document.getElementById('change').innerHTML = `<b>Change:</b> ${change}`;
 
   if (cashInt < price) {
     alert('Customer does not have enough money to purchase the item');
     return;
-  } else if (cashInt === price) {
+  }
+  if (cashInt === price) {
     displayChangeDue.innerText = 'No change due - customer paid with exact cash';
     return;
-  } else if (cash.value === "") {
+  }
+  if (cash.value === '') {
     return;
   }
 
@@ -41,7 +42,7 @@ const checkRegister = () => {
   }
 
   const denominations = [100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01];
-  const denominationNames = ["ONE HUNDRED", "TWENTY", "TEN", "FIVE", "ONE", "QUARTER", "DIME", "NICKEL", "PENNY"];
+  const denominationNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
   let changeArr = [];
   let cidCopy = [...cid];
 
@@ -63,28 +64,28 @@ const checkRegister = () => {
     return;
   }
 
-  let remainingCid = cidCopy.reduce((total, sum) => total + sum[1], 0);
+  const remainingCid = cidCopy.reduce((total, sum) => total + sum[1], 0);
   if (remainingCid === 0) {
-    displayChangeDue.innerHTML = 'Status: CLOSED ' + changeArr.map(cash => `${cash[0]}: $${cash[1].toFixed(2)}`).join(" ");
+    displayChangeDue.innerHTML = `Status: CLOSED ${changeArr.map(cash => `${cash[0]}: $${cash[1].toFixed(2)}`).join(' ')}`;
     cid = cid.map(denom => [denom[0], 0]);
   } else {
-    displayChangeDue.innerHTML = "Status: <b>OPEN</b> <br><br>" + changeArr.map(cash => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`).join(" ");
+    displayChangeDue.innerHTML = `Status: <b>OPEN</b> <br><br>${changeArr.map(cash => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`).join('')}`;
     cid = cidCopy;
   }
 
   displayCashInDrawer();
-}
+};
 
 const displayCashInDrawer = () => {
-  displayCid.innerHTML = "<h4>Cash in Drawer:</h4>" + cid.map(cash => `${cash[0]}: $${cash[1].toFixed(2)} <br>`).reverse().join("");
-}
+  displayCid.innerHTML = `<h4>Cash in Drawer:</h4>${cid.map(cash => `${cash[0]}: $${cash[1].toFixed(2)} <br>`).reverse().join('')}`;
+};
 
 window.onload = displayCashInDrawer;
 
-purchaseBtn.addEventListener("click", checkRegister);
+purchaseBtn.addEventListener('click', checkRegister);
 
-cash.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
+cash.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
     checkRegister();
   }
 });
