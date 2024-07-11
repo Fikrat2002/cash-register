8215ddb4d2311d3b32c9dec6da573249a39e5d04
fikratkhatira <fekratkhatira@gmail.com>
2024-07-10 18:25:58 -0700

    js error fixed

--- script.js
+++ script.js
@@ -50,8 +50,9 @@ const checkRegister = () => {
     let totalDenom = 0;
     while (change >= denominations[i] && cidCopy[cidCopy.length - 1 - i][1] > 0) {
       cidCopy[cidCopy.length - 1 - i][1] = Number(
-      (cidCopy[cidCopy.length - 1 - i][1] - denominations[i])
-      .toFixed(2));
+        (cidCopy[cidCopy.length - 1 - i][1] - denominations[i])
+          .toFixed(2)
+        );
       change = Number((change - denominations[i]).toFixed(2));
       totalDenom += denominations[i];
     }
